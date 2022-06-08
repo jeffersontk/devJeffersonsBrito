c6670f7d3279f9c0b0eed77b6d2e8f75e6243fe1
Jefferson Silva Brito Cabral <jscabral@firjan.com.br>
2022-06-08 20:50:03 -0300

    ajuste no link do footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Download, Envelope, GithubLogo, InstagramLogo, LinkedinLogo, Phone } from "phosphor-react";
 import { ReactElement } from "react";
 
@@ -8,18 +9,24 @@ export const Footer = (): ReactElement => {
       className="w-full bg-primary-dark h-52 flex flex-col justify-center items-center px-32">
       <div className="h-32 flex justify-end items-start gap-5 text-primary-light">
         <div className="flex flex-col gap-3">
-          <a href="https://github.com/jeffersontk" className="flex gap-1 items-end" target="_blank">
-            <GithubLogo size={24} color="#f5f5f5" weight="fill" />
-            Github
-          </a>
-          <a href="https://www.linkedin.com/in/jeffersonsbrito/" className="flex gap-1 items-end" target="_blank">
-            <LinkedinLogo size={24} color="#f5f5f5" weight="fill" />
-            Linkedin
-          </a>
-          <a href="https://www.instagram.com/jeffthebrito/" className="flex gap-1 items-end" target="_blank">
-            <InstagramLogo size={24} color="#f5f5f5" weight="fill" />
-            Instagram
-          </a>
+          <Link href="https://github.com/jeffersontk" passHref>
+            <a className="flex gap-1 items-end" target="_blank" rel="noopener noreferrer">
+              <GithubLogo size={24} color="#f5f5f5" weight="fill" />
+              Github
+            </a>
+          </Link>
+          <Link href="https://www.linkedin.com/in/jeffersonsbrito/" passHref>
+            <a className="flex gap-1 items-end" target="_blank" rel="noopener noreferrer">
+              <LinkedinLogo size={24} color="#f5f5f5" weight="fill" />
+              Linkedin
+            </a>
+          </Link>
+          <Link href="https://www.instagram.com/jeffthebrito/" passHref>
+            <a className="flex gap-1 items-end" target="_blank" rel="noopener noreferrer">
+              <InstagramLogo size={24} color="#f5f5f5" weight="fill" />
+              Instagram
+            </a>
+          </Link>
         </div>
         <div className="flex flex-col gap-3">
           <span className="flex gap-1 items-end">
